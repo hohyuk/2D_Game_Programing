a1c497c0870854ff145f5740fb3bcb2b0302f4cc
hohyuk <ghgur91@naver.com>
2017-11-03 21:45:18 +0900

    04

--- 2D_Project/MyGame.py
+++ 2D_Project/MyGame.py
@@ -2,6 +2,6 @@ from pico2d import *
 import Game_FrameWork
 import start_state
 import logo_state
-open_canvas()
-Game_FrameWork.run(logo_state)
-close_canvas()
+#open_canvas()
+Game_FrameWork.run(start_state)
+#close_canvas()
