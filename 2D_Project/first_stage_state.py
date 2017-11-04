c1b2c92c4fd768e3b70a2bbd3b199c3d853d4ba3
hohyuk <ghgur91@naver.com>
2017-11-05 00:26:45 +0900

    05

--- 2D_Project/first_stage_state.py
+++ 2D_Project/first_stage_state.py
@@ -1,3 +1,5 @@
+import json
+import os
 from pico2d import *
 
 import Game_FrameWork
@@ -10,4 +12,46 @@ background = None
 
 
 class BackGround:
+    def __init__(self):
+        self.image = load_image('image\stage\Stage1_01.png')
+
+    def draw(self):
+        self.image.clip_draw(0, 0, 800,600,400,300)
+
+
+def enter():
+    global background
+    background = BackGround()
+
+
+def exit():
+    global background
+    del background
+
+
+def update():
+    pass
+
+
+def draw():
+    clear_canvas()
+    background.draw()
+    update_canvas()
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            Game_FrameWork.quit()
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+                Game_FrameWork.quit()
+
+
+def pause():
+    pass
+
+
+def resume():
     pass
