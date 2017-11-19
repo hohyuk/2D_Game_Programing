0bcf6b68cb48ba4d62afe1083fae901f1e065bb9
hohyuk <ghgur91@naver.com>
2017-11-19 14:15:59 +0900

    1119

--- 2D_Project/Player.py
+++ 2D_Project/Player.py
@@ -5,6 +5,13 @@ import Game_FrameWork
 
 
 class Player:
+    PIXEL_PER_METER = (10.0 / 0.7)          # 10 pixel 70cm
+    FLY_SPEED_KMPH = 50.0
+    FLY_SPEED_MPM = (FLY_SPEED_KMPH * 1000.0 / 60.0)
+    FLY_SPEED_MPS = (FLY_SPEED_MPM / 60.0)
+    FLY_SPEED_PPS = (FLY_SPEED_MPS * PIXEL_PER_METER)
+
+
     STAND, FORWARD, RIGHT, LEFT = 0, 1, 2, 3
 
     image = None
@@ -17,12 +24,21 @@ class Player:
             Player.image = load_image('image\player\player.png')
 
     def handle_event(self, event):
+        #위
         if (event.type, event.key) == (SDL_KEYDOWN, SDLK_UP):
             if self.state in (self.STAND, self.LEFT, self.RIGHT):
                 self.state = self.FORWARD
                 self.yMove = 1
-        if (event.type, event.key) == (SDL_KEYUP, SDLK_UP):
-            if self.state in (self.FORWARD):
+        elif (event.type, event.key) == (SDL_KEYUP, SDLK_UP):
+            if self.state in (self.FORWARD,):
+                self.state = self.STAND
+                self.yMove = 0
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_DOWN):
+            if self.state in (self.STAND, self.LEFT, self.RIGHT):
+                self.state = self.FORWARD
+                self.yMove = -1
+        elif (event.type, event.key) == (SDL_KEYUP, SDLK_DOWN):
+            if self.state in (self.FORWARD,):
                 self.state = self.STAND
                 self.yMove = 0
         pass
@@ -34,7 +50,9 @@ class Player:
         self.frame = (self.frame + 1) % 3
         self.x += self.xMove
         self.y += self.yMove
+
         self.x = clamp(0, self.x, Game_FrameWork.Width)
+        self.y = clamp(0, self.y, Game_FrameWork.Height)
         pass
 
     def draw(self):
