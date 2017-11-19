06e0b23f3bb1e257ca9f6ec9626994ffa026191e
hohyuk <ghgur91@naver.com>
2017-11-20 00:24:51 +0900

    1120

--- 2D_Project/Player.py
+++ 2D_Project/Player.py
@@ -5,42 +5,68 @@ import Game_FrameWork
 
 
 class Player:
+    PLAYER_SIZE = 64
+
     PIXEL_PER_METER = (10.0 / 0.7)          # 10 pixel 70cm
     FLY_SPEED_KMPH = 50.0
     FLY_SPEED_MPM = (FLY_SPEED_KMPH * 1000.0 / 60.0)
     FLY_SPEED_MPS = (FLY_SPEED_MPM / 60.0)
     FLY_SPEED_PPS = (FLY_SPEED_MPS * PIXEL_PER_METER)
 
-
     STAND, FORWARD, RIGHT, LEFT = 0, 1, 2, 3
 
     image = None
+
     def __init__(self):
         self.frame = 0
         self.state = self.STAND
         self.x, self.y = 400, 90
-        self.xMove, self. yMove= 0, 0
+        self.xDir, self. yDir = 0, 0
+
         if Player.image == None :
             Player.image = load_image('image\player\player.png')
 
     def handle_event(self, event):
-        #위
+        # 위
         if (event.type, event.key) == (SDL_KEYDOWN, SDLK_UP):
             if self.state in (self.STAND, self.LEFT, self.RIGHT):
                 self.state = self.FORWARD
-                self.yMove = 1
+                self.yDir = 1
         elif (event.type, event.key) == (SDL_KEYUP, SDLK_UP):
-            if self.state in (self.FORWARD,):
+            if self.state in (self.FORWARD, self.LEFT, self.RIGHT):
                 self.state = self.STAND
-                self.yMove = 0
+                self.xDir = 0
+                self.yDir = 0
+        # 아래
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_DOWN):
             if self.state in (self.STAND, self.LEFT, self.RIGHT):
-                self.state = self.FORWARD
-                self.yMove = -1
-        elif (event.type, event.key) == (SDL_KEYUP, SDLK_DOWN):
-            if self.state in (self.FORWARD,):
                 self.state = self.STAND
-                self.yMove = 0
+                self.yDir = -1
+        elif (event.type, event.key) == (SDL_KEYUP, SDLK_DOWN):
+            if self.state in (self.STAND,):
+                self.state = self.STAND
+                self.xDir = 0
+                self.yDir = 0
+        # 왼쪽
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_LEFT):
+            if self.state in (self.STAND, self.FORWARD, self.RIGHT):
+                self.state = self.LEFT
+                self.xDir = -1
+        elif (event.type, event.key) == (SDL_KEYUP, SDLK_LEFT):
+            if self.state in (self.LEFT, self.FORWARD):
+                self.state = self.STAND
+                self.xDir = 0
+                self.yDir = 0
+        # 오른쪽
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_RIGHT):
+            if self.state in (self.STAND, self.FORWARD, self.LEFT):
+                self.state = self.RIGHT
+                self.xDir = 1
+        elif (event.type, event.key) == (SDL_KEYUP, SDLK_RIGHT):
+            if self.state in (self.RIGHT,):
+                self.state = self.STAND
+                self.xDir = 0
+                self.yDir = 0
         pass
 
     def update(self, frame_time):
@@ -48,12 +74,13 @@ class Player:
             return max(minPos, min(x, maxPos))
 
         self.frame = (self.frame + 1) % 3
-        self.x += self.xMove
-        self.y += self.yMove
+        self.x += self.xDir
+        self.y += self.yDir
 
-        self.x = clamp(0, self.x, Game_FrameWork.Width)
-        self.y = clamp(0, self.y, Game_FrameWork.Height)
+        self.x = clamp((Player.PLAYER_SIZE / 2), self.x, Game_FrameWork.Width-(Player.PLAYER_SIZE / 2))
+        self.y = clamp((Player.PLAYER_SIZE / 2), self.y, Game_FrameWork.Height-(Player.PLAYER_SIZE / 2))
         pass
 
     def draw(self):
-        self.image.clip_draw(self.frame * 64, 0, 64, 64,self.x, self.y)
+        self.image.clip_draw(self.frame * Player.PLAYER_SIZE, self.state * Player.PLAYER_SIZE
+                             , Player.PLAYER_SIZE, Player.PLAYER_SIZE,self.x, self.y)
